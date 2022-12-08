cf5907f8c5759f458c3e610a93f3c678998cc8b8
temasaur <temasaur@gmail.com>
2022-12-08 14:37:18 +0500

    make debug console easier to use

---
+++ debug/Debug.cs
@@ -0,0 +1,17 @@
+﻿namespace flightgear_interface;
+
+
+public static class Debug
+{
+	public static bool ShowDebug
+	{
+		set => DebugInterface = value ? new DebugConsole() : new EmptyDebug();
+	}
+
+	private static IDebug? DebugInterface { get; set; }
+
+	public static void WriteLine(string any)
+	{
+		DebugInterface!.WriteLine(any);
+	}
+}
